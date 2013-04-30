55375c4104a851f1cc84b1dfa380b067f5aa7eb9
Denis Kuznetsov <hokum.gru@gmail.com>
2013-04-30 18:38:41 +0400

    results intermediate changes

--- www/results.php
+++ www/results.php
@@ -1,7 +1,20 @@
 <?
 require_once 'simple_html_dom.php';
-$html = file_get_html('http://www.tripadvisor.ru/Search?q='.$_GET['q']);
-echo $html->plaintext;
-foreach($html->find('img') as $element) { //выборка всех тегов img на странице
-       echo $element->src . '<br>'; // построчный вывод содержания всех найденных атрибутов src
+$html = file_get_html('http://www.tripadvisor.ru/Search?q='.urlencode($_GET[q]));
+foreach($html->find('div.pagination') as $element){
+	echo $element->plaintext . '<br>';
 }
+echo '<hr>';
+foreach($html->find('div.searchResult div.srHead a') as $element) { //выборка всех тегов img на странице
+       $href = $element->href;
+		$matches = preg_split('/-/', $href);
+		//else echo 'no';//'^\/#[^-]*#-#[^-]*#-#[^-]*#-#[^-]*#-#[^$]#'
+		$href = '/Hotel_Review-'.$matches[1].'-'.$matches[2].'-Reviews-'.$matches[4].'-'.$matches[5];
+		print_r($matches);
+       echo $href . '<br>ddd<br>';
+}
+/*
+g298507-d2210614-Reviews-Apple_Hostel-St_Petersburg_Northwestern_District.html
+g298507-d2210614-r146471689-Apple_Hostel-St_Petersburg_Northwestern_District.html
+ * 
+ */
